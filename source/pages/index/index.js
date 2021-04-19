f6ffc7a13cf8b445729869c6f66fdc6078c839a2
GeorgeSemenov <zhora.sem@yandex.ru>
2021-04-19 15:29:10 +0300

    before lesson

--- source/pages/index/index.js
+++ source/pages/index/index.js
@@ -1,5 +1,3 @@
 import './index.scss';
 import '../../JS-components/libs.js';
 import '../../blocks/header/header.js';
-import '../../blocks/cards-screen/cards-screen.js';
-import '../../blocks/footer/footer.js';
